6d2bb97452dc3ab123275f0c21fe4b21845b0bfb
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2022-06-11 19:17:34 +0530

    init

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
-import axios from "./axios";
+import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:5001/ecommerce-webapp-c76ae/us-central1/api", //The API URL (Cloud Function using NodeJs)
